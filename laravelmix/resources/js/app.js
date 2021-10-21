3d617d5b32c087fc433e244b7ad7d873aa3c7402
Jhon <jmoreno1@iesebre.com>
2021-10-21 14:04:13 +0200

    Exemple de laravel mix

---
+++ laravelmix/resources/js/app.js
@@ -0,0 +1,5 @@
+require('./bootstrap');
+
+import user from './user.js';
+user('pepe@').consoleme()
+console.log('prova 2');
---
+++ webpack/dist/app.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(function(o="jmorneo1@"){let e=o;return{email:e,consoleme:function(){console.log("Usuari: "+e)}}})("pepe@").consoleme(),console.log("prova")})();
