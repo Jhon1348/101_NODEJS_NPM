bfb52c4101cdb9e1efc924d1924873e468f77d4e
Jhon <jmoreno1@iesebre.com>
2021-10-21 14:25:15 +0200

    Exemple Vue

---
+++ vite-project/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import user from './user.js';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Mon1!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+user('pepe@').consoleme()
+console.log('prova 2');
